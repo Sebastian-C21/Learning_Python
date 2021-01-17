1facd41762846ed74e8155fa3e230301194f28ee
Sebastian-C21 <sebastian981211@gmail.com>
2021-01-17 12:59:54 -0500

    fourth commit

--- Essentials/loops/lotharcollatz.py
+++ Essentials/loops/lotharcollatz.py
@@ -8,7 +8,7 @@ def LotharCollatz(c0):
         step = 0
         while c0 != 1:
             if c0 % 2 == 0:
-                 c0 = c0 / 2
+                c0 = c0 / 2
             elif c0 % 2 == 1:
                 c0 =  3 * c0 + 1
             step += 1
